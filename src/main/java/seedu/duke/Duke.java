56c88b0b1e36f30f04e04a88f4f45d0b468f0da0
zhenglong1603 <139528731+zhenglong1603@users.noreply.github.com>
2025-08-08 00:06:16 +0800

    Edit gradle.yml

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,6 +7,7 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
